d9c6d0f9724e2b8cc7ba5d36f34e94f19e856741
Thais Cailet <thais.cailet@gmail.com>
2018-04-28 04:29:00 -0500

    Sets up server.

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
 var path = require("path");
+//documentation available at https://github.com/expressjs/body-parser
+var bodyParser = require('body-parser');
 var app = require("express")();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'/app/public')));
+//Static file setup
+app.set('views', path.join(__dirname,'/app/public/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname,'/app/public')));
+app.use('/css',express.static(path.join(__dirname, '/app/public/assets/style.css')));
+app.use('/js',express.static(path.join(__dirname, '/public/assets/script.js')));
+app.use('/css/bootstrap',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css/bootstrap.css')));
+app.use('/js/bootstrap',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js/bootstrap.bundle.js')));
+
+//Routing
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+//Server setup
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,function(){
+    console.log("Server connected",PORT);
+});

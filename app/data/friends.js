ea3e8c94dcfa02193e93104a7c5b1d08865bdaa4
Thais Cailet <thais.cailet@gmail.com>
2018-04-28 04:14:55 -0500

    #4 Adds sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,12 @@
 [
+{"name":"flenn riley","photo":"https://randomuser.me/api/portraits/med/men/49.jpg","scores":[0,0,2,1,2,4,4,3,2,4]}, 
+{"name":"enni justi","photo":"https://randomuser.me/api/portraits/med/women/63.jpg","scores":[0,2,0,1,3,2,3,1,3,2]}, 
+{"name":"alexis cooper","photo":"https://randomuser.me/api/portraits/med/women/57.jpg","scores":[1,4,2,3,4,3,4,1,2,4]}, 
+{"name":"maelya morin","photo":"https://randomuser.me/api/portraits/med/women/81.jpg","scores":[2,1,0,0,3,1,3,4,0,0]}, 
+{"name":"nicoline jensen","photo":"https://randomuser.me/api/portraits/med/women/50.jpg","scores":[2,0,3,1,1,0,3,2,3,0]}, 
+{"name":"necati eronat","photo":"https://randomuser.me/api/portraits/med/men/85.jpg","scores":[3,3,0,4,0,2,0,0,4,3]}, 
+{"name":"luana jesus","photo":"https://randomuser.me/api/portraits/med/women/81.jpg","scores":[4,0,2,3,2,2,4,0,4,0]}, 
+{"name":"judith schiller","photo":"https://randomuser.me/api/portraits/med/women/48.jpg","scores":[1,0,1,0,2,4,1,2,3,3]}, 
+{"name":"grace thomas","photo":"https://randomuser.me/api/portraits/med/women/60.jpg","scores":[3,3,1,0,1,4,0,4,3,3]}, 
+{"name":"nicole henry","photo":"https://randomuser.me/api/portraits/med/women/2.jpg","scores":[4,1,2,4,1,4,3,0,2,1]}
 ]
